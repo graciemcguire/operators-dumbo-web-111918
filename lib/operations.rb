699f83e19b60ef0b2d21d048ab814d3d44b8bc51
Gracie McGuire <m.graciemcguire@gmail.com>
2018-10-11 18:32:28 +0000

    automatically backed up by learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,11 +1,16 @@
 def unsafe?(speed)
-
+  if speed > 60
+    return true
+  else 
+    return false
+  end
 end
 
 
 
 def not_safe?(speed)
-	
+  speed > 
+	#conditional ? action_if_true : action_if_false
 end
 	
 
